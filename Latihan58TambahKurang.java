336a6607f7adb8360bcd6ddf46cf38424f66e874
Rizqy05 <145950226+Rizqy05@users.noreply.github.com>
2023-11-30 21:12:39 +0700

    Add files via upload

---
+++ Latihan58TambahKurang.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117048.latihan58.tambahkurang;
+
+/**
+ *
+ * @author 
+ * Nama : Rizqy Mulya Saputra
+ * Nim : 22205007 
+ * Kelas : PBO 
+ * Deskripsi Program : Program ini berisi tentang menghitung jumlah dan selisih
+ *                     pada suatu bilangan.
+ */
+public class Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       JumlahBilangan bil = new JumlahBilangan();
+       bil.tampilHasilJumlah();
+       
+       SelisihBilangan blg = new SelisihBilangan();
+       blg.tampilSelisih();
+    }
+    
+}
